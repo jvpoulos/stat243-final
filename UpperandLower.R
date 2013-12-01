f76f46d3eb643af35cacac9b81ac359b7a33a24a
Steven <stchang22@yahoo.com>
2013-11-30 22:59:03 -0800

    Main functions

--- UpperandLower.R
+++ UpperandLower.R
@@ -1,38 +1,55 @@
 ### Code to create Upper and Lower Hulls###
+
+## createLowHull ##
+# This function will return a dataframe with the information
+# needed to plot the line segments.
+## Inputs: 'T' is the set/vector of abscissae
+##         'h' is the log of the target density
+##         'D' is the vector that specifies the domain
+## Output: A dataframe with the following
+##         'm' is the vector of slopes of the line segments between two abscissae
+##         'b' is the vector of y-intercepts of the line segments
+##         'left' is the vector of left starting points of each line segment
+##         'right' is the vector of right ending points of each line segment
+
 createLowHull = function(T, h, D) {
   
-  LowerBound = list()
-  for (i in 1:(length(T)-1)) {
-    LowerBound[i] = list()
-    LowerBound[i]$m = (h(T[i+1]) - h(T[i]))/(T[i+1] - T[i])
-    LowerBound[i]$b = (T[i+1] * h(T[i]) - T[i] * h(T[i+1]))/(T[i+1] - T[i])
-    LowerBound[i]$left = T[i]
-    LowerBound[i]$right = T[i+1]
-  }
   m = (h(T[-1]) - h(T[-length(T)]))/(T[-1] - T[-length(T)])
   b = (T[-1] * h(T[-length(T)]) - T[-length(T)] * h(T[-1]))/(T[-1] - T[-length(T)])
   left = T[-length(T)]
   right = T[-1]
-  LowerBound = data.frame(c(m,b,left,right))
+  LowerBound = data.frame(cbind(m,b,left,right))
   colnames(LowerBound) = c('m', 'b', 'left', 'right')
   
   return(LowerBound)
 }
 
 
-
+## createUpHull ##
+# This function will return the necessary information to find the line segments that
+# make up the upper boundary.
+## Inputs: 'T' is the set/vector of abscissae
+##         'h' is the log of the target density
+##         'D' is the vector that specifies the domain
+## Output: a dataframe with the following information
+##         'm' is the vector of slopes of the line segments between two intersection points
+##         'b' is the vector of y-intercepts of the line segments
+##         'left' is the vector of left starting points of each line segment
+##         'right' is the vector of right ending points of each line segment
+##         'prob' is the integral of the exponential function raised to the line segment.
 createUpHull = function(T, h, D) {
   
   
   x = T
-  m = numericDeriv(quote(h(x)),'x') # slopes
+  m = diag(attr(numericDeriv(quote(h(x)),'x'),'gradient')) # slopes
   b = h(T) - m*T # y-ints
   z = (b[-1] - b[-length(b)])/(m[-length(m)] - m[-1]) # intersection points
   prob0 = exp(b)/m * (exp(m * c(z, D[2])) - exp(m * c(D[1],z))) #possibly unnormalized probabilities
   prob = prob0/sum(prob0) # normalized probabilities
+  prob[is.nan(prob)] = 1
   left = c(D[1],z) # left endpoint for each line segment
   right = c(z, D[2]) # right endpoint for each line segment
-  UpBound = data.frame(cbind(m,b,z,prob,left,right))
-  colnames(UpBound) = c('m', 'b', 'z', 'prob', 'left','right')
+  UpBound = data.frame(cbind(m,b,prob,left,right))
+  colnames(UpBound) = c('m', 'b', 'prob', 'left','right')
   return(UpBound)
 }
