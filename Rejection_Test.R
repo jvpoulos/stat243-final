5cf6c948c7495e082347ba47af640031f6037d56
Willy Lai <wlai@Willys-MacBook-Pro.local>
2013-12-04 19:46:48 -0800

    blah

---
+++ Rejection_Test.R
@@ -0,0 +1,26 @@
+#This is a rough draft of the code for the rejection sampling test.
+
+#The function rejectiontest returns 3 logicals of whether we accept the sample,
+#have to update h(x), and checks if the function h(x) is log-concave at x_star.  
+#If it is not log-concave, then the function is supposed to stop.
+
+#Suppose x_star is a sample from s_k
+
+w = runif(0,1)
+rejectiontest = function(x_star,w){
+  A = w <= exp(l_k(x_star)-u_k(x_star)) | w <= exp(h(x_star)-u_k(x_star)) #logical for acceptance
+  Up = !(w <= exp(l_k(x_star)-u_k(x_star))) #logical for update
+  if Up{
+    logconcave = l_k(x_star) <= h(x_star) & h(x_star) <= u_k(x_star)
+  }
+  else{
+    logconcave = TRUE
+  }
+  return(c(A,Up,logconcave))
+}
+
+if !logconcave{
+  stop('Error: The target density is not log-concave.')
+}
+
+
