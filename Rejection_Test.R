5c6f6fbf6e7c7904d2ca0a923e5c4a1b0f609af4
Steven Chang <stchang22@yahoo.com>
2013-12-12 16:32:55 -0800

    comments

--- Rejection_Test.R
+++ Rejection_Test.R
@@ -5,14 +5,23 @@
 #'
 #' @name rejectiontest
 #' 
-#' @title This is a rough draft of the code for the rejection sampling test.
+#' @title This code will perform the rejection algorithm.
 #' 
 #' @description
 #'The function rejectiontest returns 3 logicals of whether we accept the sample,
 #'have to update h(x), and checks if the function h(x) is log-concave at x_star.  
 #'If it is not log-concave, then the function is supposed to stop.
 #'
-#'Suppose x_star is a sample from s_k
+#' @param x_star sample candidate point
+#' @param w a number from the Uniform(0,1) distribution
+#' @param l_k evaluation of lower bound at x_star
+#' @param u_k evaluation of upper bound at x_star
+#' @param h log of target density
+#' 
+#' Output is a logical vector of three elements
+#' @return A TRUE if we accept x_star
+#' @return Up TRUE if we need to update the abscissae
+#' @return logconcave TRUE if log-concave at x_star
 
 w = runif(0,1)
 rejectiontest = function(x_star,w, l_k, u_k, h){
